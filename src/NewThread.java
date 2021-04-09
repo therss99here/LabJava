843aba5ca3c8aa1416adf25784f1707395baa3f6
therss99here <rahulsinghsengar009@gmail.com>
2021-04-10 00:23:10 +0530

    minor tweaks with output

--- src/NewThread.java
+++ src/NewThread.java
@@ -10,7 +10,7 @@ public class NewThread implements Runnable {
     public void run() {
         try {
             for(int i = 5; i>0; i--) {
-                System.out.println("Thread working: " + i);
+                System.out.println("Thread " + name +" working: " + i);
                 Thread.sleep(500);
             }
         } catch (InterruptedException e) {
